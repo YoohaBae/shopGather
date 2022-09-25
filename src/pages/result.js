c3a597949fbde29ea662184b4d26f5415d3639f4
Yool Bi Lee <yoolbi716@gmail.com>
2022-09-25 03:15:56 -0400

    tabs

--- src/pages/result.js
+++ src/pages/result.js
@@ -1,6 +1,33 @@
-import { Button, Tabs } from "@mui/material";
+import { Button, Tabs, Tab, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
 
 const Result = () => {
   const [listOfObject, setListOfObject] = useState(
@@ -9,6 +36,12 @@ const Result = () => {
   const [listOfPerson, setListOfPerson] = useState(
     JSON.parse(window.sessionStorage.getItem("people"))
   );
+  const [selectedTab, setSelectedTab] = useState(0);
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <div>
@@ -20,6 +53,22 @@ const Result = () => {
           height="30"
           style={{ margin: "15px" }}
         />
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs
+            onChange={handleChange}
+            value={value}
+            aria-label="basic tabs example"
+          >
+            {listOfPerson.map((person) => (
+              <Tab label={person.name} key={person.id} />
+            ))}
+          </Tabs>
+        </Box>
+        {listOfPerson.map((person) => (
+          <TabPanel value={value} index={person.id} key={person.id}>
+            {person.name}
+          </TabPanel>
+        ))}
       </Box>
     </div>
   );
