d9b0dc344aed85f7e05dda93de35b6e700a9266a
Yool Bi Lee <yoolbi716@gmail.com>
2022-09-24 21:12:06 -0400

    finished uploading image page

--- src/pages/uploadImage.js
+++ src/pages/uploadImage.js
@@ -4,10 +4,15 @@
   } from "@mui/material";
   import { Box } from "@mui/system";
   import React, {useState} from "react";
-//   import { Link } from "react-router-dom";
+  import { Link } from "react-router-dom";
   
   const UploadImage = () => {
-    const [uploaded, setUploaded] = useState(false);
+    const [extension, setExtension] = useState("");
+
+    const handleImage = (e) => {
+        console.log(e.target.value);
+        setExtension(e.target.value);
+    }
 
     return (
       <div>
@@ -26,15 +31,34 @@
                 justifyContent="center"
                 alignItems="center"
                 flexDirection="column"
-                sx={{height:"90%"}}
+                sx={{height:"85%"}}
             >
                 <Button variant="outlined" sx={{width: "250px", marginBottom: "15px"}}>Take Photo of Receipt</Button>
                 <Button variant="contained" component="label" sx={{width: "250px"}}>
                     Upload Reciept
-                    <input hidden accept="image/*" multiple type="file" />
-
+                    <input hidden accept="image/*" type="file" onChange={handleImage}/>
                 </Button>
+                <div style={{marginTop: "5px"}}>{extension}</div>
             </Box>
+            {(extension.includes("png") || extension.includes("jpg") || extension.includes("jpeg")) ? 
+                <div style={{width: "100%", justifyContent: "right"}}>
+                    <Button 
+                    size="large"
+                    sx={{float: "right"}}
+                    ><Link to={`/selectImageType`} style={{ color: "inherit" }}>
+                        Next
+                    </Link></Button>
+                </div>
+                :
+                <div style={{width: "100%", justifyContent: "right"}}>
+                    <Button 
+                    size="large"
+                    sx={{float: "right"}}
+                    ><Link to={`/setPerson`} style={{ color: "inherit" }}>
+                        Next
+                    </Link></Button>
+                </div>
+            }
         </Box>
         
       </div>
